b370eb979a9d67ee403aaa74e6d6dcbb5855e768
AntonioFlores1 <af513249@gmail.com>
2019-01-20 00:41:31 -0500

    VC set up and constraits also Network Service file set up

---
+++ WeatherApp/WeatherApp/Cells/DailyCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  DailyCollectionViewCell.swift
+//  WeatherApp
+//
+//  Created by Pursuit on 1/20/19.
+//  Copyright © 2019 Pursuit. All rights reserved.
+//
+
+import UIKit
+
+class DailyCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var DateLabel: UILabel!
+    
+    @IBOutlet weak var forcastImage: UIImageView!
+    
+    @IBOutlet weak var forcastLabel: UILabel!
+}
---
+++ WeatherApp/WeatherApp/Controllers/WeatherApi.swift
@@ -0,0 +1,9 @@
+//
+//  WeatherApi.swift
+//  WeatherApp
+//
+//  Created by Pursuit on 1/20/19.
+//  Copyright © 2019 Pursuit. All rights reserved.
+//
+
+import Foundation
