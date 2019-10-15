89aefb88fb4f8204079e8fc72f087a6df1c8b117
AngelaMae_Garrovillas <androiduser1757@gmail.com>
2019-10-15 06:47:14 -0400

    adds weather label

--- WeatherApp/WeatherApp/Controllers/ViewController.swift
+++ WeatherApp/WeatherApp/Controllers/ViewController.swift
@@ -10,6 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var weatherLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Weather Forcast for "
+        label.textAlignment = .center
+        label.textColor = .black
+        return label
+    }()
     var weatherCollectionView: UICollectionView = {
         let collection = UICollectionView(frame: CGRect(x: 0, y: 0, width: 414, height: 200), collectionViewLayout: UICollectionViewFlowLayout())
         collection.register(ForcastCell.self, forCellWithReuseIdentifier: "forcastCell")
@@ -18,13 +25,21 @@ class ViewController: UIViewController {
     }()
     
     private func setupUI() {
+        setupWeatherLabel()
         setupWeatherCollectionView()
+    }
+    private func setupWeatherLabel() {
+        view.addSubview(weatherLabel)
+        weatherLabel.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            weatherLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor,constant: 10),
+            weatherLabel.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor)])
     }
     private func setupWeatherCollectionView() {
         view.addSubview(weatherCollectionView)
         weatherCollectionView.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
-            weatherCollectionView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            weatherCollectionView.topAnchor.constraint(equalTo: weatherLabel.bottomAnchor,constant: 10),
             weatherCollectionView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
             weatherCollectionView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
             weatherCollectionView.heightAnchor.constraint(equalToConstant: weatherCollectionView.frame.height),
