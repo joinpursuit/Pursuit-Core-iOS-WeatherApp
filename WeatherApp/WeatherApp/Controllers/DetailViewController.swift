a3b4093a929df58e1fc0792a79869605f17e6518
AngelaMae_Garrovillas <androiduser1757@gmail.com>
2019-10-15 17:54:26 -0400

    adds weather label for detailVC

--- WeatherApp/WeatherApp/Controllers/DetailViewController.swift
+++ WeatherApp/WeatherApp/Controllers/DetailViewController.swift
@@ -12,16 +12,35 @@ class DetailViewController: UIViewController {
     //MARK: - Properties
     var dailyWeather: DataWrapper?
     var name: String?
+    
+    lazy var weatherLabel: UILabel = {
+        let label = UILabel()
+        if let name = self.name, let dailyWeather = self.dailyWeather {
+            label.text = "Weather Forcast in \(name) on \(dailyWeather.convertTimeToDate(time: dailyWeather.time))"
+        }
+        label.textAlignment = .center
+        return label
+    }()
     var detailStackView = WeatherStackView()
     
+    //MARK: - Constraints
     private func setupDetailUI() {
         setupStackView()
+        setupWeatherLabel()
     }
     private func setupStackView() {
         view.addSubview(detailStackView)
         NSLayoutConstraint.activate([
             detailStackView.centerYAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerYAnchor),
             detailStackView.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor)])
+    }
+    private func setupWeatherLabel() {
+        view.addSubview(weatherLabel)
+        weatherLabel.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            weatherLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor,constant: 15),
+            weatherLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            weatherLabel.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor)])
     }
 
     override func viewDidLoad() {
