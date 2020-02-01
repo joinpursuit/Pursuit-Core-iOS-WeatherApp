d9c5ee44011f83ae249115196d046b4e087b0813
Yuliia Engman <yuliia.engman@gmail.com>
2020-02-01 13:20:26 -0500

    created WeatherModel

---
+++ WeatherApp/WeatherApp/WeatherAPIClient.swift
@@ -0,0 +1,11 @@
+//
+//  WeatherAPIClient.swift
+//  WeatherApp
+//
+//  Created by Yuliia Engman on 2/1/20.
+//  Copyright © 2020 David Rifkin. All rights reserved.
+//
+
+import Foundation
+import NetworkHelper
+
