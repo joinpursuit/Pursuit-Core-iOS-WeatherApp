3007f00f0ca257884689543bd0693748b8973dfc
Ahad Islam <ahad.islam624@gmail.com>
2020-02-05 17:55:02 -0500

    created a custom collection view cell created a collection view constrained the view

---
+++ WeatherApp/WeatherApp/Controllers/ForecastCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  ForecastCollectionViewCell.swift
+//  WeatherApp
+//
+//  Created by Ahad Islam on 2/5/20.
+//  Copyright © 2020 David Rifkin. All rights reserved.
+//
+
+import UIKit
+
+class ForecastCollectionViewCell: UICollectionViewCell {
+    
+}
--- WeatherApp/WeatherApp/Controllers/ViewController.swift
+++ WeatherApp/WeatherApp/Controllers/ViewController.swift
@@ -9,12 +9,63 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private lazy var collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .horizontal
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.register(ForecastCollectionViewCell.self, forCellWithReuseIdentifier: "Forecast Cell")
+        return collectionView
+        
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureView()
+    }
+    
+    private func configureView() {
         view.backgroundColor = .systemOrange
+        
+        setupCollectionView()
+    }
+    
+    private func setupCollectionView() {
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        
+        view.addSubview(collectionView)
+        
+        collectionView.backgroundColor = .systemGray
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            collectionView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            collectionView.heightAnchor.constraint(equalToConstant: view.frame.height / 3)])
     }
 
 
+}
+
+extension ViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Forecast Cell", for: indexPath) as? ForecastCollectionViewCell else {
+            print("Error occured creating cell")
+            return UICollectionViewCell()
+        }
+        return cell
+    }
+    
+}
+
+extension ViewController: UICollectionViewDelegate {}
+
+extension ViewController: UICollectionViewDelegateFlowLayout {
+    
 }
 
